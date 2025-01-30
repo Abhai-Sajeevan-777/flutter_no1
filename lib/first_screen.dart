86b7eb7d26968357023dfe2807ef881fbdf69f59
Abhai-Sajeevan-777 <abhaisajeevan73@gmail.com>
2025-01-30 11:31:45 +0530

    ..

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -10,24 +10,42 @@ class FirstScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Color(0xff776655),
         leading: Icon(Icons.arrow_back),
-        title: Text("Instagram"),
+        title: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Text("Instagram"),
+        ),
         actions: [
-          Icon(Icons.more),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Icon(Icons.more),
+          ),
           Icon(Icons.search),
+          SizedBox(
+            width: 20,
+          ),
           Icon(Icons.camera_alt),
+          SizedBox(
+            width: 20,
+          ),
           Icon(Icons.logout),
+          SizedBox(
+            width: 20,
+          ),
           Icon(Icons.notification_add)
         ],
       ),
       body: SafeArea(
         child: Center(
-            child: Text(
-          "Abhai",
-          style: TextStyle(
-              fontSize: 50,
-              color: const Color.fromARGB(255, 67, 45, 215),
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.bold),
+            child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Text(
+            "Abhaisdfghjkjhgfdwertyuizxcvbnmhgfwefghjkdfghjfghjghjghjktghjghjghjghjfghjrtyuijhgfdsdfghbvcrtyuikl",
+            style: TextStyle(
+                fontSize: 50,
+                color: const Color.fromARGB(255, 67, 45, 215),
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold),
+          ),
         )),
       ),
     );
